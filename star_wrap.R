db67f24174b03d6204ab0ba5fbc22155a2347fdf
laupierre <laupierre@users.noreply.github.com>
2023-03-11 23:17:41 +0100

    Create

---
+++ star_wrap.R
@@ -0,0 +1,23 @@
+library (openxlsx)
+library (DESeq2)
+library (ggplot2)
+
+system ("mkdir ./output")
+system ("cp ./projects/log.out ./output")
+
+anno <- read.delim ("gencode.v43.annotation.txt")
+anno <- anno[ ,grep ("transcript_id", colnames (anno), invert=TRUE)]
+anno <- unique (anno)
+
+a <- read.delim ("./projects/star_results/subread.counts.txt", skip=1)
+a <- a[ ,grep ("ene|bam", colnames (a))]
+a <- a[grep ("miRNA|Mt_tRNA|Mt_rRNA|rRNA|snRNA|snoRNA|scRNA|sRNA|misc_RNA|scaRNA|ribozyme|IG_|TR_", a$gene_type, invert=TRUE), ]
+colnames (a) <- gsub ("_S[0-9]+.*", "", colnames (a)) 
+colnames (a) <- gsub (".*IIT_", "", colnames (a))
+
+a <- merge (a, anno, by.x="Geneid", by.y="gene_id", all.x=TRUE) 
+a <- a[ ,grep ("gene_type.y|gene_name.y", colnames (a), invert=TRUE)]
+colnames (a) [colnames (a) == "gene_name.x"] <- "gene_name"
+colnames (a) [colnames (a) == "gene_type.x"] <- "gene_type"
+  
+write.xlsx (a, "./output/star_gene_raw_counts.xlsx", rowNames=F)
