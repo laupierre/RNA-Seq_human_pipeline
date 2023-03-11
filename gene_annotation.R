7dd66797789c005892ea9529af423cf94df64402
laupierre <laupierre@users.noreply.github.com>
2023-03-11 23:14:40 +0100

    Create

---
+++ gene_annotation.R
@@ -0,0 +1,28 @@
+library(rtracklayer)
+library(biomaRt)
+
+system ("wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_43/gencode.v43.annotation.gtf.gz")
+
+my_obj <- import("gencode.v43.annotation.gtf.gz")
+my_obj <- as.data.frame (my_obj)
+my_obj <- my_obj[my_obj$type == "transcript", ]
+my_obj <- my_obj[ ,c("gene_id", "transcript_id", "gene_type", "gene_name", "hgnc_id")]
+
+
+ensembl <- useEnsembl(biomart = 'genes', 
+                       dataset = 'hsapiens_gene_ensembl',
+                       version = 109)
+
+values <- unique (my_obj$gene_id)
+length (values)
+# 62703
+
+res <- getBM(attributes = c('ensembl_gene_id_version', 'external_gene_name', 'description'),
+             filters = 'ensembl_gene_id_version',
+             values = values, mart = ensembl)      
+dim (res)
+# 62656
+
+my_obj <- merge (my_obj, res, by.x="gene_id", by.y="ensembl_gene_id_version", all.x=TRUE)
+
+write.table (my_obj, "gencode.v43.annotation.txt", sep="\t", quote=F, row.names=F)
