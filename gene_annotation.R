c2a8c7dc7abce3dcd4acbb2adc6df78351ff741f
laupierre <laupierre@users.noreply.github.com>
2023-07-20 10:10:20 +0200

    Update gene_annotation.R

--- gene_annotation.R
+++ gene_annotation.R
@@ -8,21 +8,22 @@ my_obj <- as.data.frame (my_obj)
 my_obj <- my_obj[my_obj$type == "transcript", ]
 my_obj <- my_obj[ ,c("gene_id", "transcript_id", "gene_type", "gene_name", "hgnc_id")]
 
+values <- unique (my_obj$gene_id)
+length (values)
+# 62703
 
 ensembl <- useEnsembl(biomart = 'genes', 
                        dataset = 'hsapiens_gene_ensembl',
                        version = 109)
 
-values <- unique (my_obj$gene_id)
-length (values)
-# 62703
-
-res <- getBM(attributes = c('ensembl_gene_id_version', 'external_gene_name', 'description'),
+res <- getBM(attributes = c('ensembl_gene_id_version', 'external_gene_name', 'chromosome_name', 'start_position' , 'end_position', 'strand', 'description'),
              filters = 'ensembl_gene_id_version',
              values = values, mart = ensembl)      
 dim (res)
 # 62656
 
 my_obj <- merge (my_obj, res, by.x="gene_id", by.y="ensembl_gene_id_version", all.x=TRUE)
+dim (my_obj)
+# 62703
 
 write.table (my_obj, "gencode.v43.annotation.txt", sep="\t", quote=F, row.names=F)
