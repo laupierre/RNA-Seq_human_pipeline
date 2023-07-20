d75eebad9a5e5d150ad745e10dd8a77c3b406821
laupierre <laupierre@users.noreply.github.com>
2023-07-20 10:01:24 +0200

    Update star_wrap.R

--- star_wrap.R
+++ star_wrap.R
@@ -8,9 +8,15 @@ system ("cp ./projects/log.out ./output")
 anno <- read.delim ("gencode.v43.annotation.txt")
 anno <- anno[ ,grep ("transcript_id", colnames (anno), invert=TRUE)]
 anno <- unique (anno)
+dim (anno)
+# 62703
 
 a <- read.delim ("./projects/star_results/subread.counts.txt", skip=1)
 a <- a[ ,grep ("ene|bam", colnames (a))]
+length (intersect (a$Geneid, anno$gene_id))
+# 62703
+
+# remove RNAs
 a <- a[grep ("miRNA|Mt_tRNA|Mt_rRNA|rRNA|snRNA|snoRNA|scRNA|sRNA|misc_RNA|scaRNA|ribozyme|IG_|TR_", a$gene_type, invert=TRUE), ]
 colnames (a) <- gsub ("_S[0-9]+.*", "", colnames (a)) 
 colnames (a) <- gsub (".*IIT_", "", colnames (a))
